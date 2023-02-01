afd6d6ad01951b997dacb881044ea8c8c7941a97
David Gallmeier <dg@xounges.net>
2023-02-01 16:37:46 -0500

    feat: add swagger-ui

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Rust RestAPI
 
 This is a basic web app written in rust, using the actix-web framework.
+
+## References
+
+* actix-web examples: <https://github.com/actix/examples>
+* utoipa <https://github.com/juhaku/utoipa>
+    * examples: <https://github.com/juhaku/utoipa/tree/master/examples>
+      * utoipa-ui: <https://docs.rs/utoipa-swagger-ui/latest/utoipa_swagger_ui/>
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,93 +1,83 @@
 use std::{error::Error, net::Ipv4Addr};
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, middleware::Logger};
 
-mod my_api {
-    use actix_web::{get, post, HttpResponse, Responder};
-    use utoipa::ToSchema;
+use actix_web::{App, get, HttpResponse, HttpServer, middleware::Logger, post, Responder, web};
+use actix_web::dev::JsonBody;
+use actix_web::middleware::ErrorHandlerResponse::Response;
+use serde::{Deserialize, Serialize};
+use utoipa::{ToSchema, OpenApi};
+use utoipa_swagger_ui::SwaggerUi;
 
-    #[utoipa::path(
-        get,
-        path = "/",
-        responses(
-            (status = 200, description = "Hello world!"),
-            (status = NOT_FOUND, description = "not found!")
-        )
-    )]
-    #[get("/")]
-    pub async fn hello() -> impl Responder {
-        HttpResponse::Ok().body("Hello world!")
-    }
-
-    #[utoipa::path(
-        post,
-        path = "/echo",
-        responses(
-            (status = 200, description = "request body received."),
-            (status = "5XX", description = "server error")
-        ),
-        request_body(content = String, description = "json as string request", content_type = "application/json")
-    )]
-    #[post("/echo")]
-    pub async fn echo(req_body: String) -> impl Responder {
-        HttpResponse::Ok().body(req_body)
-    }
-
-    pub async fn manual_hello() -> impl Responder {
-        HttpResponse::Ok().body("Hey there!")
-    }
+#[derive(ToSchema, Deserialize)]
+struct RequestBlob {
+    id: u64,
+    value: String,
 }
+
+#[derive(ToSchema, Serialize)]
+struct ResponseBlob {
+    id: u64,
+    value: String,
+}
+
+#[utoipa::path(
+    get,
+    path = "/",
+    responses(
+        (status = 200, description = "Hello world!"),
+        (status = NOT_FOUND, description = "not found!")
+    )
+)]
+#[get("/")]
+pub async fn hello() -> impl Responder {
+                                     HttpResponse::Ok().body("Hello world!")
+                                                                            }
+
+#[utoipa::path(
+    post,
+    path = "/echo",
+    request_body = HelloBlob,
+    responses(
+        (status = 200,
+            description = "hello blob received",
+            body = ResponseBlob,
+            content_type = "application/json",
+            example = json!({"id": 1123, "value": "test-value"})),
+        (status = "5XX", description = "server error")
+    )
+)]
+#[post("/echo")]
+async fn echo(hello_blob: web::Json<RequestBlob>) -> actix_web::Result<impl Responder> {
+    let response_blob = ResponseBlob {
+        id: hello_blob.id,
+        value: hello_blob.value.to_string()
+    };
+    Ok(web::Json(response_blob))
+}
+
+async fn manual_hello() -> impl Responder {
+                                            HttpResponse::Ok().body("Hey there!")
+    }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    use utoipa::OpenApi;
-
     #[derive(OpenApi)]
-    #[openapi(paths(my_api::hello, my_api::echo))]
+    #[openapi(paths(hello, echo), components(schemas(RequestBlob, ResponseBlob)))]
     struct ApiDoc;
 
-    println!("{}", ApiDoc::openapi().to_pretty_json().unwrap());
+    // println!("{}", ApiDoc::openapi().to_pretty_json().unwrap());
 
     HttpServer::new(|| {
         App::new()
-            .service(my_api::hello)
-            .service(my_api::echo)
-            .route("/hey", web::get().to(my_api::manual_hello))
+            .service(hello)
+            .service(echo)
+            .service(
+                SwaggerUi::new("/swagger-ui/{_:.*}")
+                    .url("/api-doc/openapi.json", ApiDoc::openapi()),
+            )
+            .route("/hey", web::get().to(manual_hello))
     })
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
 }
 
-
-mod pet_api {
-    use utoipa::ToSchema;
-
-    #[derive(ToSchema)]
-    pub struct Pet {
-        id: u64,
-        name: String,
-        age: Option<i32>,
-    }
-
-    /// Get pet by id
-    ///
-    /// Get pet from database by pet id
-    #[utoipa::path(
-    get,
-    path = "/pets/{id}",
-    responses(
-    (status = 200, description = "Pet found succesfully", body = Pet),
-    (status = NOT_FOUND, description = "Pet was not found")
-    ),
-    params(
-    ("id" = u64, Path, description = "Pet database id to get Pet for"),
-    )
-    )]
-    async fn get_pet_by_id(pet_id: u64) -> Pet {
-        Pet {
-            id: pet_id,
-            age: None,
-            name: "lightning".to_string(),
-        }
-    }
-}
