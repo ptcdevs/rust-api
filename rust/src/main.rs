3989292e4ceee8546eb155e486d85e470b34f6f9
David Gallmeier <dg@xounges.net>
2023-02-01 17:00:12 -0500

    Update webapp and release pr action

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,10 +1,10 @@
 use std::{error::Error, net::Ipv4Addr};
 
-use actix_web::{App, get, HttpResponse, HttpServer, middleware::Logger, post, Responder, web};
 use actix_web::dev::JsonBody;
 use actix_web::middleware::ErrorHandlerResponse::Response;
+use actix_web::{get, middleware::Logger, post, web, App, HttpResponse, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
-use utoipa::{ToSchema, OpenApi};
+use utoipa::{OpenApi, ToSchema};
 use utoipa_swagger_ui::SwaggerUi;
 
 #[derive(ToSchema, Deserialize)]
@@ -19,44 +19,37 @@ struct ResponseBlob {
     value: String,
 }
 
-#[utoipa::path(
-    get,
-    path = "/",
+#[utoipa::path(get, path = "/",
     responses(
-        (status = 200, description = "Hello world!"),
-        (status = NOT_FOUND, description = "not found!")
-    )
-)]
+    (status = 200, description = "Hello world!"),
+    (status = NOT_FOUND, description = "not found!")
+    ))]
 #[get("/")]
 pub async fn hello() -> impl Responder {
-                                     HttpResponse::Ok().body("Hello world!")
-                                                                            }
+    HttpResponse::Ok().body("Hello world!")
+}
 
-#[utoipa::path(
-    post,
-    path = "/echo",
-    request_body = HelloBlob,
+#[utoipa::path( post, path = "/echo",
+    request_body = RequestBlob,
     responses(
         (status = 200,
             description = "hello blob received",
             body = ResponseBlob,
             content_type = "application/json",
-            example = json!({"id": 1123, "value": "test-value"})),
-        (status = "5XX", description = "server error")
-    )
-)]
+            example = json ! ({"id": 1123, "value": "test-value"})),
+        (status = "5XX", description = "server error")))]
 #[post("/echo")]
 async fn echo(hello_blob: web::Json<RequestBlob>) -> actix_web::Result<impl Responder> {
     let response_blob = ResponseBlob {
         id: hello_blob.id,
-        value: hello_blob.value.to_string()
+        value: hello_blob.value.to_string(),
     };
     Ok(web::Json(response_blob))
 }
 
 async fn manual_hello() -> impl Responder {
-                                            HttpResponse::Ok().body("Hey there!")
-    }
+    HttpResponse::Ok().body("Hey there!")
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -76,8 +69,7 @@ async fn main() -> std::io::Result<()> {
             )
             .route("/hey", web::get().to(manual_hello))
     })
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
-
