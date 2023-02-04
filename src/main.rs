b470667e9f43c104f3c6a982b4d7454d3b330aad
David Gallmeier <dg@xounges.net>
2023-02-03 22:38:12 -0500

    feat: configure persistent cookie session

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ mod config;
 use std::borrow::Borrow;
 use std::env;
 use std::sync::Arc;
-use actix_web::cookie::Key;
+use actix_session::{
+    config::PersistentSession, storage::CookieSessionStore, Session, SessionMiddleware,
+};
+use actix_session::config::SessionLifecycle::BrowserSession;
+use actix_session::config::{SessionMiddlewareBuilder, TtlExtensionPolicy};
+use actix_web::cookie::{self, Key};
 use actix_web::dev::JsonBody;
 use actix_web::middleware::ErrorHandlerResponse::Response;
-use actix_web::{App, error, Error, get, HttpResponse, HttpServer, middleware::ErrorHandlerResponse, middleware::Logger, post, Responder, web};
+use actix_web::{App, error, Error, get, HttpRequest, HttpResponse, HttpServer, middleware::ErrorHandlerResponse, middleware::Logger, post, Responder, web};
 use serde::{Deserialize, Serialize};
 use actix_web::web::Redirect;
 use reqwest::StatusCode;
@@ -16,6 +21,7 @@ use utoipa_swagger_ui::SwaggerUi;
 use crate::github_oauth::github_oauth::{GithubOauthConfig, GithubOauthConfigBorrowed};
 use confy::{ConfyError, load_path};
 use config::AppConfig;
+use rand::{distributions::Alphanumeric, Rng}; // 0.8
 
 #[derive(ToSchema, Deserialize)]
 struct RequestBlob {
@@ -59,38 +65,53 @@ async fn manual_hello() -> impl Responder {
 (status = FOUND, description = "found"),
 (status = 5XX, description = "server error")))]
 #[get("/login")]
-pub async fn login(github_oauth: web::Data<GithubOauthConfig>) -> actix_web::Result<impl Responder, Error> {
-    let github_authorize = github_oauth.get_authorize_url();
-    //TODO: pass state to user session, then extract on /callback
-    Ok(Redirect::to(github_authorize.0).using_status_code(StatusCode::FOUND))
+pub async fn login(session: Session, github_oauth: web::Data<GithubOauthConfig>) -> actix_web::Result<impl Responder, Error> {
+    let github_authorize_url = github_oauth.get_authorize_url();
+    let session_insert = session.insert("state", github_authorize_url.1)?;
+    Ok(Redirect::to(github_authorize_url.0).using_status_code(StatusCode::FOUND))
+}
+
+#[utoipa::path(get, path = "/callback", responses(
+(status = OK, description = "ok"),
+(status = 5XX, description = "server error")))]
+#[get("/callback")]
+pub async fn callback(request: HttpRequest, session: Session, github_oauth: web::Data<GithubOauthConfig>) -> actix_web::Result<impl Responder, Error> {
+    let state: Option<String> = session.get("state")?;
+    Ok(HttpResponse::Ok().body("Hello world!"))
 }
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
     #[derive(OpenApi)]
-    #[openapi(paths(hello, echo, manual_hello, login), components(schemas(RequestBlob, ResponseBlob)))]
+    #[openapi(paths(hello, echo, manual_hello, login, callback), components(schemas(RequestBlob, ResponseBlob)))]
     struct ApiDoc;
 
-    let config: AppConfig = confy::load_path("config/local/config.yaml").expect("failure reading github creds");
-    let github_secret = env::var("GITHUB_OAUTH_CLIENT_SECRET").expect("missing github client secret from environment variables");
-    let github_config = GithubOauthConfig {
-        client_id: config.github_oauth.client_id,
-        client_secret: github_secret,
-        redirect_url: config.github_oauth.redirect_url,
-        scopes: config.github_oauth.scopes,
-    };
+    HttpServer::new(|| {
+        let config: AppConfig = confy::load_path("config/local/config.yaml").expect("failure reading github creds");
+        let github_secret = env::var("GITHUB_OAUTH_CLIENT_SECRET").expect("missing github client secret from environment variables");
+        let github_config = GithubOauthConfig {
+            client_id: config.github_oauth.client_id,
+            client_secret: github_secret,
+            redirect_url: config.github_oauth.redirect_url,
+            scopes: config.github_oauth.scopes,
+        };
 
-    HttpServer::new(move || {
         App::new()
+            .wrap(
+                SessionMiddleware::builder(CookieSessionStore::default(), Key::from(&[0; 64]))
+                    .cookie_secure(false)
+                    .session_lifecycle(PersistentSession::default().session_ttl(cookie::time::Duration::hours(2)))
+                    .build())
             .service(hello)
             .service(echo)
             .service(login)
+            .service(callback)
             .service(
                 SwaggerUi::new("/swagger-ui/{_:.*}")
                     .url("/api-doc/openapi.json", ApiDoc::openapi()),
             )
             .route("/hey", web::get().to(manual_hello))
-            .app_data(web::Data::new(github_config.clone()))
+            .app_data(web::Data::new(github_config))
     })
         .bind(("127.0.0.1", 8080))?
         .run()
