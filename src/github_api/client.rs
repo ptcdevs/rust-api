5280360bc78507768637000e5d38af32f3743f90
David Gallmeier <dg@xounges.net>
2023-02-07 00:04:13 -0500

    Update parser

--- src/github_api/client.rs
+++ src/github_api/client.rs
@@ -5,6 +5,8 @@ use crate::github_api::client::client::GithubClient;
 
 pub mod client {
     use std::collections::HashMap;
+    use futures::{StreamExt, TryFutureExt, TryStreamExt};
+    use futures::stream::Collect;
     use crate::error::MyError;
 
     #[derive(Default, Debug)]
@@ -15,9 +17,9 @@ pub mod client {
     }
 
     impl GithubClient {
-        pub fn new(access_token_response: &str) {
+        pub fn new(access_token_response: &str) -> Result<HashMap<&str, &str>, MyError> {
             // eg: access_token=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&scope=repo%2Cuser&token_type=bearer
-            let elements = access_token_response
+            let elements: Vec<Result<(&str,&str),MyError>> = access_token_response
                 .split("&")
                 .map(|elems| {
                     let mut elem_split = elems
@@ -30,12 +32,23 @@ pub mod client {
                         .ok_or(MyError::TokenResponseParseError)?;
 
                     Ok((key,value))
-                });
-            let kv_pair = elements
+                })
+                .collect();
+            let kv_pair: Vec<(&str,&str)> = elements
+                .clone()
+                .into_iter()
                 .filter(|kv| kv.is_ok())
-                .map(|kv: Result<(&str,&str),MyError>| kv.unwrap());
-            let parsed: HashMap<&str,&str> = HashMap::from_iter(kv_pair);
-            todo!()
+                .map(|kv: Result<(&str,&str),MyError>| kv.unwrap())
+                .collect();
+                // .map_ok_or_else(|kv: Result<(&str,&str),MyError>| kv.unwrap(), MyError::TokenResponseParseError);
+
+            let parsed: Result<HashMap<&str,&str>,MyError> = if kv_pair.clone().into_iter().count() == 3 {
+                Ok(HashMap::from_iter(kv_pair))
+            }  else {
+                Err(MyError::TokenResponseParseError)
+            };
+
+            parsed
         }
     }
 }
